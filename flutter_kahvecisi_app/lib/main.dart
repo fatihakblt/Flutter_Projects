7fec317938cbe33b29b9932a65da5bd592bdf791
Fatih Akbulut <fatihakbulut997@gmail.com>
2023-08-30 23:01:26 +0300

    Uygulama ikonu oluşturuldu.

---
+++ flutter_kahvecisi_app/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(BenimUyg());
+}
+class BenimUyg extends StatelessWidget {
+  const BenimUyg({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        body: Center(),
+
+      ),
+    );
+  }
+}
+
+
